07718eb736b47354cdb97875f93713eef3771ad7
Junior Oliver Lozano Gutierrez <U23216557@utp.edu.pe>
2026-04-27 00:35:14 -0500

    fix: eliminar marcas de conflicto accidentales en SecurityConfig

--- src/main/java/com/ONG/web/Config/DataInitializer.java
+++ src/main/java/com/ONG/web/Config/DataInitializer.java
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 package com.ONG.web.Config;
 
 import com.ONG.web.Model.Administrador;
 import com.ONG.web.Model.Usuario;
 import com.ONG.web.Repository.AdministradorRepository;
 import com.ONG.web.Repository.UsuarioRepository;
-=======
-package com.UnidosCorazones.demo.Config;
-
-import com.UnidosCorazones.demo.Model.Administrador;
-import com.UnidosCorazones.demo.Model.Usuario;
-import com.UnidosCorazones.demo.Respository.AdministradorRepository;
-import com.UnidosCorazones.demo.Respository.UsuarioRepository;
->>>>>>> ba0f054 (feat: implementar controladores web y configuracion del sistema)
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,10 +11,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class DataInitializer {
-<<<<<<< HEAD
-
-=======
->>>>>>> ba0f054 (feat: implementar controladores web y configuracion del sistema)
     @Bean
     CommandLineRunner initDatabase(AdministradorRepository administradorRepository, PasswordEncoder passwordEncoder) {
         return args -> {
@@ -34,11 +21,8 @@ public class DataInitializer {
                 admin.setNombre("Super");
                 admin.setApellido("Admin");
                 admin.setCorreo("admin@ong.com");
-<<<<<<< HEAD
-                // Spring encripta la contraseña antes de guardar
-=======
+
                 // AQUÍ está la magia: Spring encripta la contraseña antes de guardar
->>>>>>> ba0f054 (feat: implementar controladores web y configuracion del sistema)
                 admin.setContrasena(passwordEncoder.encode("admin123"));
                 admin.setTelefono("999888777");
                 admin.setDocIdentidad("00000001");
