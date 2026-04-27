2536c029b384aec19e4b6e81bf676670b593983f
Karla Flores <U23235289@utp.edu.pe>
2026-04-26 21:49:05 -0500

    feat: models Administrador, Usuario

---
+++ src/main/java/com/ONG/web/Model/Administrador.java
@@ -0,0 +1,23 @@
+package com.ONG.web.Model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "administrador")
+@PrimaryKeyJoinColumn(name = "id_admin")//Une la PK de esta tabla con Usuario
+@Getter
+@Setter
+public class Administrador extends Usuario {
+    @Column(length = 100)
+    private String cargo;
+
+    @OneToMany(mappedBy = "administrador", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference("admin-campanias")
+    private Set<Campania> campanias;
+
+}
